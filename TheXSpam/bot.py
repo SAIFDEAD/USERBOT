4c7585cac0eafdc2389e36feddc885ae41329d98
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-11 01:45:18 +0530

    Update bot.py

--- TheXSpam/bot.py
+++ TheXSpam/bot.py
@@ -1,4 +1,4 @@
-# 𝑪𝒐𝒑𝒚𝒓𝒊𝒈𝒉𝒕 𝑩𝒚 𝑨𝒍𝒕𝒓𝒐𝒏
+# 𝑪𝒐𝒑𝒚𝒓𝒊𝒈𝒉𝒕 𝑩𝒚 𝑆𝑎𝑖𝑓𝑑𝑒𝑎𝑑
 # 𝑨𝒍𝒍 𝑹𝒊𝒈𝒉𝒕𝒔 𝑹𝒆𝒔𝒆𝒓𝒗𝒆𝒅
 
 import sys
@@ -11,21 +11,21 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-ALT = f"""❖ 𝐏𝐘 𝐀𝐋𝐓𝐑𝐎𝐍 ❖
+ALT = f"""❖ 𝙎𝘼𝙄𝙁 𝙐𝙎𝙀𝙍𝘽𝙊𝙏 𝙎𝙔𝙎𝙏𝙀𝙈  ❖
 
-➠ **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `3.11.1`
-➠ **ᴘʏʀᴏ ᴠᴇʀsɪᴏɴ** : `1.4.16`
-➠ **xꜱᴘᴀᴍ ᴠᴇʀsɪᴏɴ**  : `3.3`
-➠ **ᴜᴘᴅᴀᴛᴇꜱ** : @TheAltron\n"""
+➠ **𝐏ʏᴛʜᴏɴ 𝐕ᴇʀsɪᴏɴ** : `3.11.1`
+➠ **𝐏ʏʀᴏ 𝐕ᴇʀsɪᴏɴ** : `1.4.16`
+➠ **𝐒ᴀɪғ 𝐔sᴇʀʙᴏᴛ 𝐕ᴇʀsɪᴏɴ**  : `3.3`
+➠ **𝐔ᴘᴅᴀᴛᴇꜱ** : @SAIFALLBOT\n"""
 
 
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["ping"], ["/", ".", "!"]))
 async def ping(_, e: Message):       
       start = datetime.datetime.now()
-      Fuk = await e.reply("**Pong !!**")
+      Fuk = await e.reply("**𝗦𝗔𝗜𝗙 𝗢𝗣**")
       end = datetime.datetime.now()
       ms = (end-start).microseconds / 1000
-      await Fuk.edit_text(f"◇ ᴛʜᴇ ᴀʟᴛʀᴏɴ\n◇ ᴘɪɴɢ: `{ms}ms`\n◇ ᴠᴇʀsɪᴏɴ: `3.3`")
+      await Fuk.edit_text(f"𝐀ʟɪᴠᴇ 𝐌ʏ [𝐌ᴀsᴛᴇʀ](https://t.me/SAIFALLBOT)")
 
 
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["alive"], ["/", ".", "!"]))
@@ -38,7 +38,7 @@ async def alive(xspam: Client, msg: Message):
 
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["reboot", "restart"], ["/", ".", "!"]))
 async def restart_bot(_, message: Message):
-    msg = await message.reply("`ʀᴇsᴛᴀʀᴛɪɴɢ ʙᴏᴛ...`")
+    msg = await message.reply("`ʀᴇsᴛᴀʀᴛɪɴɢ ʙᴏᴛ sᴀɪғ ᴜsᴇʀ ʙᴏᴛ...`")
     args = [sys.executable, "main.py"]
-    await msg.edit("» ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ...\n» ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ʙᴏᴛ ᴀғᴛᴇʀ 𝟷 ᴍɪɴᴜᴛᴇ ")
+    await msg.edit("» ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ...\n» ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ sᴀɪғ ᴜsᴇʀ ʙᴏᴛ ᴀғᴛᴇʀ 𝟷 ᴍɪɴᴜᴛᴇ ")
     execle(sys.executable, *args, environ)
