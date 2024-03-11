b6943dfa0ba6c5e921cad0d9b434d0a7128a337d
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-11 20:03:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from config import API_ID, API_HASH, SESSIONS
 from pyrogram import Client, idle
 
 
-def load_plugins(plugin_name):
-    path = Path(f"TheXSpam/{plugin_name}.py")
-    spec = importlib.util.spec_from_file_location(f"TheXSpam{plugin_name}", path
-    sys.TheXSpam["TheXSpam." + plugin_name] = load
-    print("King X spam   has Imported " + plugin_name)
+CLIENTS = []
+
+for SESSION in SESSIONS:
+    if SESSION:
+        client = Client(
+            session_name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            plugins=dict(root="TheXSpam"),
+        )
+        CLIENTS.append(client)
 
 
 if __name__ == "__main__":
@@ -14,11 +20,11 @@ if __name__ == "__main__":
     for i, CLIENT in enumerate(CLIENTS):
         try:
             CLIENT.start()
-            CLIENT.join_chat("SAIFHELPGC")
             CLIENT.join_chat("SAIFALLBOT")
+            CLIENT.join_chat("SAIFHELPGC")
             print(f"---> Client {i+1} has been Started...")
         except Exception as e:
             print(e)
 
-    print("💞 YOUR SAIF USER-BOTDEPLOYED SUCCESSFULLY 💞")
+    print("💞YOUR SAIF USERBOTS DEPLOYED SUCCESSFULLY 💞")
     idle()
