f56b2fa4b90e5c948508834543aea5af7f2780a6
Bhadvaheroku <156423343+Bhadvaheroku@users.noreply.github.com>
2024-03-27 22:11:46 +0530

    Update openai.py

--- X/modules/user/openai.py
+++ X/modules/user/openai.py
@@ -15,35 +15,27 @@ import random
 
 from .help import *
 
-@kaz.on_message(filters.command("cask", cmd) & filters.user(DEVS) & ~filters.me)
-@kaz.on_message(filters.command("ai", cmd) & filters.me)
+@Client.on_message(filters.command("cask", ".") & filters.user(DEVS) & ~filters.me)
+@Client.on_message(filters.command("ai", ".") & filters.me)
 async def openai(client: Client, message: Message):
     if len(message.command) == 1:
         return await message.reply(f"Ketik <code>.{message.command[0]} [question]</code> Questions for use OpenAI")
+    
     question = message.text.split(" ", maxsplit=1)[1]
+    
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {OPENAI_API_KEY}",
     }
 
-    json_data = {
-        "model": "gpt-3.5-turbo-instruct",
-        "prompt": question,
-        "max_tokens": 200,
-        "temperature": 0,
-    }
+    url = f"https://chatgpt.apinepdev.workers.dev/?question={question}"
+    
     msg = await message.reply("`Be patient..")
+    
     try:
-        response = (await http.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
-        await msg.edit(response["choices"][0]["text"])
+        response = requests.get(url).json()
+        await msg.edit(response["answer"])
     except MessageNotModified:
         pass
     except Exception:
         await msg.edit("An Error Has Occurred!!\nYou Have Not Entered OPENAI_API_KEY")
-
-add_command_help(
-    "openai",
-    [
-        ["ai", "To Ask Something To Google"]
-    ],
-) 
