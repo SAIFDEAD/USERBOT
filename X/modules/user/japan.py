84a5d6070882146dcc2a994ddd090e5f4ca22fd5
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-03-28 11:31:05 +0530

    Create japan.py

---
+++ X/modules/user/japan.py
@@ -0,0 +1,7 @@
+from aiohttp.client_exceptions import ClientError
+from pyrogram import filters, Client 
+from pyrogram.types import Message
+
+
+from X.helpers.aiohttp_helper import AioHttp
+from .help import *
