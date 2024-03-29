424a8460dd67439ca0758abac541886cf8dcc82b
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-03-29 16:53:58 +0530

    Create __main__.py

---
+++ X/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from X.helper import join
+from X.modules import ALL_MODULES
+from X import clients, app, ids
+
+async def start_bot():
+    await app.start()
+    print("LOG: Founded Bot token Booting..")
+    for all_module in ALL_MODULES:
+        importlib.import_module("X.modules" + all_module)
+        print(f"Successfully Imported {all_module} 💥")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            await join(cli)
+            print(f"Started {ex.first_name} 🔥")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
