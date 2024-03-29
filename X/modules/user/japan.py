b89b7e5354a1fdc411459b0ec255383b382e0db6
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-03-29 12:13:48 +0530

    Delete X/modules/user/japan.py

--- X/modules/user/japan.py
+++
@@ -1,7 +0,0 @@
-from aiohttp.client_exceptions import ClientError
-from pyrogram import filters, Client 
-from pyrogram.types import Message
-
-
-from X.helpers.aiohttp_helper import AioHttp
-from .help import *
