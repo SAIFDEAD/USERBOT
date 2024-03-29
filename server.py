20f8b13d33225107b69417906b086f5398b2d4b0
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-03-29 18:24:25 +0530

    ᴊᴀᴘᴀɴᴇꜱᴇ 3.0

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return "ꜱᴛᴏʀᴍ ɪꜱ ʀᴜɴɴɪɴɢ"
+        return "ᴊᴀᴘᴀɴᴇꜱᴇ ɪꜱ ʀᴜɴɴɪɴɢ"
 
 api.add_resource(Greeting, '/')
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
