74ec56ef5128daa42733561ade0fb44c0d3c9af9
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-03-29 11:39:48 +0530

    Create powers.py

---
+++ X/powers.py
@@ -0,0 +1,274 @@
+from pyrogram.types import Message, User
+from pyrogram import Client, enums
+import os
+import sys
+from re import sub
+from time import time
+import aiohttp
+import requests
+import asyncio
+import math
+import os
+import shlex
+from typing import Tuple
+from PIL import Image
+from pymediainfo import MediaInfo
+import time
+
+startTime = time.time()
+
+BASE = "https://batbin.me/"
+
+async def get_id_and_args(client, message):
+    if message.reply_to_message:
+        id = message.reply_to_message.from_user.id
+        if len(message.command) != 1:
+            args = message.text.split(None, 1)[1]
+        else:
+            args = None
+    else:
+        if len(message.command) > 1:
+            if len(message.command) < 3:
+                args = None
+            else:
+                args = message.text.split(None, 2)[2]
+            try:
+                id = int(message.text.split()[1])
+            except:
+                pass
+        if message.entities:
+            if len(message.command) < 3:
+                args = None
+            else:
+                args = message.text.split(None, 2)[2]
+            for x in message.entities:
+              if x.type.name == "MENTION":
+                txt = message.text.split()[1]
+                if txt[0] != "@":
+                    id = None
+                else:
+                    try:
+                        id = (await client.get_users(txt)).id
+                    except:
+                        id = None
+              elif x.type.name == "TEXT_MENTION":
+                id = x.user.id
+    return id, args
+
+async def get_id(client, message: Message):
+    if str(message.chat.id)[0] != "-":
+        return int(message.chat.id)
+    if not message.reply_to_message:
+        text = message.text.split()
+        un_or_id = text[1]
+        if un_or_id[0] == "@":
+            id = (await client.get_users(un_or_id)).id
+        else:
+            id = int(un_or_id)
+    else:
+        id = message.reply_to_message.from_user.id
+    return id
+
+def ReplyCheck(message: Message):
+    reply_id = None
+
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.id
+
+    elif not message.from_user.is_self:
+        reply_id = message.id
+
+    return reply_id
+
+
+
+admins_in_chat = {}
+
+async def list_admins(client: Client, chat_id: int):
+    global admins_in_chat
+    if chat_id in admins_in_chat:
+        interval = time() - admins_in_chat[chat_id]["last_updated_at"]
+        if interval < 3600:
+            return admins_in_chat[chat_id]["data"]
+
+    admins_in_chat[chat_id] = {
+        "last_updated_at": time(),
+        "data": [
+            member.user.id
+            async for member in client.get_chat_members(
+                chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS
+            )
+        ],
+    }
+    return admins_in_chat[chat_id]["data"]
+
+
+async def extract_userid(message, text: str):
+    def is_int(text: str):
+        try:
+            int(text)
+        except ValueError:
+            return False
+        return True
+
+    text = text.strip()
+
+    if is_int(text):
+        return int(text)
+
+    entities = message.entities
+    app = message._client
+    if len(entities) < 2:
+        return (await app.get_users(text)).id
+    entity = entities[1]
+    if entity.type == "mention":
+        return (await app.get_users(text)).id
+    if entity.type == "text_mention":
+        return entity.user.id
+    return None
+
+
+async def extract_user_and_reason(message, sender_chat=False):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        if not reply.from_user:
+            if (
+                reply.sender_chat
+                and reply.sender_chat != message.chat.id
+                and sender_chat
+            ):
+                id_ = reply.sender_chat.id
+            else:
+                return None, None
+        else:
+            id_ = reply.from_user.id
+
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return id_, reason
+
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+
+    if len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+
+    return user, reason
+
+
+async def extract_user(message):
+    return (await extract_user_and_reason(message))[0]
+
+
+async def extract_args(message, markdown=True):
+    if not (message.text or message.caption):
+        return ""
+
+    text = message.text or message.caption
+
+    text = text.markdown if markdown else text
+    if " " not in text:
+        return ""
+
+    text = sub(r"\s+", " ", text)
+    text = text[text.find(" ") :].strip()
+    return text
+
+
+async def extract_args_arr(message, markdown=True):
+    return extract_args(message, markdown).split()
+
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+
+async def PasteBin(text):
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
+    return link
+
+
+async def paste(content: str):
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
+        return
+    return BASE + resp["message"]
+
+
+async def s_paste(message, extension="txt"):
+    siteurl = "https://spaceb.in/api/v1/documents/"
+    try:
+        response = requests.post(
+            siteurl, data={"content": message, "extension": extension}
+        )
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        if response["error"] != "" and response["status"] < 400:
+            return {"error": response["error"]}
+        return {
+            "url": f"https://spaceb.in/{response['payload']['id']}",
+            "raw": f"{siteurl}{response['payload']['id']}/raw",
+            "bin": "Spacebin",
+        }
+    return {"error": "Unable to reach spacebin."}
+
+
+
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+
+def get_arg(message: Message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+
+def get_args(message: Message):
+    try:
+        message = message.text
+    except AttributeError:
+        pass
+    if not message:
+        return False
+    message = message.split(maxsplit=1)
+    if len(message) <= 1:
+        return []
+    message = message[1]
+    try:
+        split = shlex.split(message)
+    except ValueError:
+        return message
+    return list(filter(lambda x: len(x) > 0, split))
