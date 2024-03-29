b1050e1176f773509a097e8202d9c2f441b36a2b
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-03-29 17:09:33 +0530

    Update __init__.py

--- X/__init__.py
+++ X/__init__.py
@@ -1,33 +1,105 @@
-from pyrogram import Client
-from config import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10
-from datetime import datetime
+import asyncio
+import logging
+import sys
 import time
-from aiohttp import ClientSession
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
+from typing import Any, Dict
 
-StartTime = time.time()
-START_TIME = datetime.now()
+
+from aiohttp import ClientSession
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from gpytranslate import Translator
+from pyrogram import Client
+from pytgcalls import GroupCallFactory
+
+
+from config import (
+    API_HASH,
+    API_ID,
+    BOTLOG_CHATID,
+    STRING_SESSION1,
+    STRING_SESSION2,
+    STRING_SESSION3,
+    STRING_SESSION4,
+    STRING_SESSION5,
+    SUDO_USERS,
+    BOT_TOKEN
+)
 CMD_HELP = {}
 SUDO_USER = SUDO_USERS
 clients = []
 ids = []
+LOG_FILE_NAME = "logs.txt"
 
-SUDO_USERS.append(OWNER_ID)
-aiosession = ClientSession()
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+logging.getLogger("pytgcalls").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
+logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
 
-if API_ID:
-   API_ID = API_ID
-else:
-   print("WARNING: API ID NOT FOUND USING ZAID API ⚡")
-   API_ID = "6435225"
 
-if API_HASH:
-   API_HASH = API_HASH
-else:
-   print("WARNING: API HASH NOT FOUND USING ZAID API ⚡")   
-   API_HASH = "4e984ea35f854762dcde906dce426c2d"
+LOGS = logging.getLogger(__name__)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+
+if (
+    not STRING_SESSION1
+    and not STRING_SESSION2
+    and not STRING_SESSION3
+    and not STRING_SESSION4
+    and not STRING_SESSION5
+):
+    LOGGER(__name__).warning("STRING SESSION NOT FOUND , SHUTDOWN BOT!")
+    sys.exit()
+
+if not API_ID:
+    LOGGER(__name__).warning("API_ID NOT FOUND, SHUTDOWN BOT")
+    sys.exit()
+
+if not API_HASH:
+    LOGGER(__name__).warning("API_HASH NOT FOUND, SHUTDOWN BOT")
+    sys.exit()
 
 if not BOT_TOKEN:
-   print("WARNING: BOT TOKEN NOT FOUND PLZ ADD ⚡")   
+   LOGGER(__name__).warning("WARNING: BOT TOKEN NOT FOUND, SHUTDOWN BOT")
+   sys.exit
+
+if BOTLOG_CHATID:
+   BOTLOG_CHATID = BOTLOG_CHATID
+else:
+   BOTLOG_CHATID = "me"
+
+LOOP = asyncio.get_event_loop()
+
+trl = Translator()
+
+aiosession = ClientSession()
+
+CMD_HELP = {}
+
+scheduler = AsyncIOScheduler()
+
+StartTime = time.time()
+
+START_TIME = datetime.now()
+
+TEMP_SETTINGS: Dict[Any, Any] = {}
+TEMP_SETTINGS["PM_COUNT"] = {}
+TEMP_SETTINGS["PM_LAST_MSG"] = {}
 
 app = Client(
     name="app",
@@ -38,52 +110,129 @@ app = Client(
     in_memory=True,
 )
 
-if STRING_SESSION1:
-   print("Client1: Found.. Starting..📳")
-   client1 = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION1, plugins=dict(root="X/modules"))
-   clients.append(client1)
+bot1 = (
+    Client(
+        name="bot1",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION1,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION1
+    else None
+)
 
-if STRING_SESSION2:
-   print("Client2: Found.. Starting.. 📳")
-   client2 = Client(name="two", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION2, plugins=dict(root="X/modules"))
-   clients.append(client2)
+bot2 = (
+    Client(
+        name="bot2",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION2,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION2
+    else None
+)
 
-if STRING_SESSION3:
-   print("Client3: Found.. Starting.. 📳")
-   client3 = Client(name="three", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION3, plugins=dict(root="X/modules"))
-   clients.append(client3)
+bot3 = (
+    Client(
+        name="bot3",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION3,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION3
+    else None
+)
 
-if STRING_SESSION4:
-   print("Client4: Found.. Starting.. 📳")
-   client4 = Client(name="four", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION4, plugins=dict(root="X/modules"))
-   clients.append(client4)
+bot4 = (
+    Client(
+        name="bot4",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION4,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION4
+    else None
+)
 
-if STRING_SESSION5:
-   print("Client5: Found.. Starting.. 📳")
-   client5 = Client(name="five", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION5, plugins=dict(root="X/modules"))
-   clients.append(client5)
+bot5 = (
+    Client(
+        name="bot5",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION5,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION5
+    else None
+)
 
-if STRING_SESSION6:
-   print("Client6: Found.. Starting.. 📳")
-   client6 = Client(name="six", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION6, plugins=dict(root="X/modules"))
-   clients.append(client6)
+bot6 = (
+    Client(
+        name="bot6",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION6,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION6
+    else None
+)
 
-if STRING_SESSION7:
-   print("Client7: Found.. Starting.. 📳")
-   client7 = Client(name="seven", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION7, plugins=dict(root="X/modules"))
-   clients.append(client7)
+bot7 = (
+    Client(
+        name="bot7",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION7,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION7
+    else None
+)
 
-if STRING_SESSION8:
-   print("Client8: Found.. Starting.. 📳")
-   client8 = Client(name="eight", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION8, plugins=dict(root="X/modules"))
-   clients.append(client8)
+bot8 = (
+    Client(
+        name="bot8",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION8,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION8
+    else None
+)
 
-if STRING_SESSION9:
-   print("Client9: Found.. Starting.. 📳")
-   client9 = Client(name="nine", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION9, plugins=dict(root="X/modules"))
-   clients.append(client9)
+bot9 = (
+    Client(
+        name="bot9",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION9,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION9
+    else None
+)
 
-if STRING_SESSION10:
-   print("Client10: Found.. Starting.. 📳")
-   client10 = Client(name="ten", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION10, plugins=dict(root="X/modules")) 
-   clients.append(client10)
+bot10 = (
+    Client(
+        name="bot10",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=STRING_SESSION10,
+        plugins=dict(root="X/modules"),
+    )
+    if STRING_SESSION10
+    else None
+)
+
+
+bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10] if bot]
+
+for bot in bots:
+    if not hasattr(bot, "group_call"):
+        setattr(bot, "group_call", GroupCallFactory(bot).get_group_call())
