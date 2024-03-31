ba545417596b3daf1ac6fa8e7e46adb02439ea38
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-03-31 21:28:36 +0530

    Update data.py

--- X/helpers/data.py
+++ X/helpers/data.py
@@ -3,7 +3,7 @@ from X import CMD_HELP
 class Data:
 
     text_help_menu = (
-        "**𝐌𝐞𝐧𝐮 𝐈𝐧𝐥𝐢𝐧𝐞 𝐉𝐚𝐩𝐚𝐧𝐞𝐬𝐞-𝐗-𝐔𝐬𝐞𝐫𝐛𝐨𝐭**\n**Prefixes:** ., ?, !, *"
+        "**𝐌𝐞𝐧𝐮 𝐈𝐧𝐥𝐢𝐧𝐞 𝐉𝐚𝐩𝐚𝐧𝐞𝐬𝐞-𝐗-𝐔𝐬𝐞𝐫𝐛𝐨𝐭**\n**𝐏𝐫𝐞𝐟𝐢𝐱𝐞𝐬:** ., ?, !, *"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
