818a6de3b7a26a80bfcfdbe4d95812000a4675ae
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-04-11 23:39:33 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

--- X/helpers/data.py
+++ X/helpers/data.py
@@ -3,10 +3,10 @@ from X import CMD_HELP
 class Data:
 
     text_help_menu = (
-        "**𝐌𝐞𝐧𝐮 𝐈𝐧𝐥𝐢𝐧𝐞 𝐉𝐚𝐩𝐚𝐧𝐞𝐬𝐞-𝐗-𝐔𝐬𝐞𝐫𝐛𝐨𝐭**\n**𝐏𝐫𝐞𝐟𝐢𝐱𝐞𝐬:** ., ?, !, *"
+        "**𝐌𝐞𝐧𝐮 𝐈𝐧𝐥𝐢𝐧𝐞 𝐃𝐢𝐜𝐭𝐚𝐭𝐨𝐫 𝐔𝐬𝐞𝐫𝐛𝐨𝐭**\n**𝐏𝐫𝐞𝐟𝐢𝐱𝐞𝐬:** ., ?, !, *"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
         .replace("'", "")
     )
-    reopen = [[InlineKeyboardButton("•─╼⃝𖠁 ꜱᴇᴇ ᴍᴏʀᴇ 𖠁⃝╾─•", callback_data="reopen")]]
+    reopen = [[InlineKeyboardButton("𖠁⃝ ꜱᴇᴇ ᴍᴏʀᴇ 𖠁⃝", callback_data="reopen")]]
