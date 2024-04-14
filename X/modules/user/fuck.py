b21664e9c8a95f967f306e701a8e4520b14f75ab
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-04-15 00:14:06 +0530

    Create fuck.py

---
+++ X/modules/user/fuck.py
@@ -0,0 +1,112 @@
+fromfrom random import choice
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from XDB.data import MASTERS, RAID
+from config import OWNER_ID
+from config import CMD_HANDLER as cmd
+from .help import *
+import asyncio
+
+Client.on_message(filters.user(SUDO_USERS) & filters.command(["fuck"], ["."]))
+async def raid(xspam: Client, message: Message):  
+    kex = message.text.split(" ")
+
+    if len(kex) > 2:
+        ok = await xspam.get_users(kex[2])  
+        id = ok.id
+        if id in MASTERS:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❣️")
+        elif id == OWNER_ID:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ 🥀")
+        else:
+            counts = int(kex[1])
+            fname = ok.first_name
+            mention = f"[{fname}](tg://user?id={id})"
+            for _ in range(counts):
+                reply = choice(RAID)
+                msg = f"{mention} {reply}"
+                await xspam.send_message(message.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+    elif message.reply_to_message and (len(kex) == 2):
+        user_id = message.reply_to_message.from_user.id
+        ok = await xspam.get_users(user_id)
+        id = ok.id
+        if id in MASTERS:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❣️")
+        elif id == OWNER_ID:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ 🥀")
+        else:
+            counts = int(kex[1])
+            fname = ok.first_name
+            mention = f"[{fname}](tg://user?id={id})"
+            for _ in range(counts):
+                reply = choice(RAID)
+                msg = f"{mention} {reply}"
+                await xspam.send_message(message.chat.id, msg)
+                await asyncio.sleep(0.3)
+
+    else:
+        await message.reply_text(".ғᴜᴄᴋ 10 <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+
+rusers = []
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["rfuck", "rraid"], ["."]))
+async def rraid(xspam: Client, message: Message):
+    global rusers
+    kex = message.text.split(" ")
+
+    if len(kex) > 1:
+        ok = await xspam.get_users(kex[1])
+        id = ok.id
+        if id in MASTERS:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❣️")
+        elif id == OWNER_ID:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ 🥀")
+        else:
+            rusers.append(id)
+            await message.reply_text("ᴀᴄᴛɪᴠᴀᴛᴇᴅ ʀᴇᴘʟʏʀᴀɪᴅ ✅")
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        if id in MASTERS:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❣️")
+        elif id == OWNER_ID:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ 🥀")
+        else:
+            rusers.append(user_id)
+            await message.reply_text("» ᴀᴄᴛɪᴠᴀᴛᴇᴅ ʀᴇᴘʟʏʀᴀɪᴅ ✅")
+
+    else:
+        await message.reply_text(".rfuck <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["dfuck", "draid", "drraid"], ["."]))
+async def draid(xspam: Client, message: Message):
+    global rusers
+    kex = message.text.split(" ")
+
+    if len(kex) > 1:
+        ok = await xspam.get_users(kex[1])
+        id = ok.id
+        if id in rusers:
+            rusers.remove(id)
+            await message.reply_text("ʀᴇᴘʟʏ ʀᴀɪᴅ ᴅᴇ-ᴀᴄᴛɪᴠᴀᴛᴇᴅ ✅")
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        ok = await xspam.get_users(user_id)
+        id = ok.id
+        if id in rusers:
+            rusers.remove(id)
+            await message.reply_text("ʀᴇᴘʟʏ ʀᴀɪᴅ ᴅᴇ-ᴀᴄᴛɪᴠᴀᴛᴇᴅ ✅")
+
+    else:
+        await message.reply_text(".dfuck <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+
+@Client.on_message(~filters.me & filters.incoming)
+async def watcher(_, msg: Message):
+    global rusers
+    id = msg.from_user.id
+    if id in rusers:
+        reply = choice(RAID)
+        await msg.reply_text(reply)
