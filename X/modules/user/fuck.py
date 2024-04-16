d11064e1f9fc0fd1dc0493b90a4572439a64714a
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-04-16 14:38:25 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

--- X/modules/user/fuck.py
+++ X/modules/user/fuck.py
@@ -1,7 +1,7 @@
 from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from XDB.data import MASTERS, RAID, SDICTATOR
+from XDB.data import MASTERS, RAID, SDICTATOR, HRAID
 from config import OWNER_ID
 from config import CMD_HANDLER as cmd
 from .help import *
@@ -111,11 +111,54 @@ async def watcher(_, msg: Message):
         reply = choice(SDICTATOR)
         await msg.reply_text(reply)
 
+@Client.on_message(filters.command("hraid", cmd) & filters.me)
+async def hraid(xspam: Client, message: Message):  
+    kex = message.text.split(" ")
+
+    if len(kex) > 2:
+        ok = await xspam.get_users(kex[2])
+        id = ok.id
+        if id in MASTERS:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❣️")
+        elif id == OWNER_ID:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ 🥀")
+        else:
+            counts = int(kex[1])
+            fname = ok.first_name
+            mention = f"[{fname}](tg://user?id={id})"
+            for _ in range(counts):
+                reply = choice(HRAID)
+                msg = f"{mention} {reply}"
+                await xspam.send_message(message.chat.id, msg)
+                await asyncio.sleep(0.3)
+
+    elif message.reply_to_message and (len(kex) == 2):
+        user_id = message.reply_to_message.from_user.id
+        ok = await xspam.get_users(user_id)
+        id = ok.id
+        if id in MASTERS:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴅᴇᴠᴇʟᴏᴘᴇʀ ❣️")
+        elif id == OWNER_ID:
+            await message.reply_text("ɴᴏᴘᴇ ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ 🥀")
+        else:
+            counts = int(kex[1])
+            fname = ok.first_name
+            mention = f"[{fname}](tg://user?id={id})"
+            for _ in range(counts):
+                reply = choice(HRAID)
+                msg = f"{mention} {reply}"
+                await xspam.send_message(message.chat.id, msg)
+                await asyncio.sleep(0.3)
+
+    else:
+        await message.reply_text(".ʜʀᴀɪᴅ 10 <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+
 add_command_help(
     "➥ 𝐅ᴜᴄᴋ ",
     [
         ["raid", "Raid to user."],
         ["rfuck", " reply fuck to user"],
         ["dfuck", "to remove rfuck to user"],
+        ["hraid", " Raid to user in hindi"],
     ],
   )
