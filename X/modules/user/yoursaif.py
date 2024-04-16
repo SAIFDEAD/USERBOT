5f962938bcb633efd6972ce5dc0ac979b9fb1e86
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-04-16 14:43:00 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

---
+++ X/modules/user/yoursaif.py
@@ -0,0 +1,8 @@
+from random import choice
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from XDB.data import MASTERS, WISH, FLIRT, SRAID, LOVERAID
+from config import OWNER_ID
+from config import CMD_HANDLER as cmd
+from .help import *
+import asyncio
