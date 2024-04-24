6a2e5b119b4ee11cd9495fda7105164c40e57352
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-04-24 20:42:36 +0530

    Update dictatoranimation.py

--- X/modules/user/dictatoranimation.py
+++ X/modules/user/dictatoranimation.py
@@ -1,1 +1,57 @@
+importimport asyncio
+import random
 
+import requests
+from pyrogram import *
+from pyrogram import Client, filters
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram.types import *
+from pyrogram.types import Message
+
+from config import CMD_HANDLER
+from X.helpers.basic import edit_or_reply, get_text
+from X.helpers.constants import MEMES
+
+from .help import * 
+
+
+Client.on_message(filters.command(["demon"], cmd) & filters.me)
+async def demon(client: Client, message: Message):
+    await edit_or_reply(
+        message,
+       ⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀/n⠀⠀⠀
+ ⠀    ⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷/n⠀
+   ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇/n
+ ⠀   ⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄⠀⣁⣀⣤⣴⣿⣿⣿⣆/n
+ ⠀⠀⠀  ⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿/n
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟/n
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿/n
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃/n
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿/n
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿/n⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁/n
+    )
+
+
+
+
+Client.on_message(filters.command(["sad"], cmd) & filters.me)
+async def sad(client: Client, message: Message):
+    await edit_or_reply(
+        message,
+        ⁣⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿/n
+        ⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿/n
+        ⁣⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿/n
+        ⁣⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿/n
+        ⁣⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿/n
+        ⁣⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿/n
+        ⁣⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿/n
+        ⁣⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿/n
+        ⁣⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿/n
+        ⁣⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿/n
+        ⁣⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿/n
+
+    )
+
+
+      
